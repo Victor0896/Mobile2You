6a14a19eafe979219b20906733a6d3e10fd4a788
victoredbc <victoredbc@Victors-MacBook-Pro.local>
2022-01-10 23:41:04 -0300

    movieCell view layout

--- TodoMovies/TodoMovies/Views/movieCell.swift
+++ TodoMovies/TodoMovies/Views/movieCell.swift
@@ -9,22 +9,66 @@ import SwiftUI
 
 struct movieCell: View {
     var body: some View {
-        HStack {
+        GeometryReader { view in
             HStack {
-                Image(systemName: "camera")
-                VStack {
-                    Text("Edward Scissorhands")
-                    HStack {
-                        Text("1992")
-                        Text("Drama, comedy")
-                    }
+                HStack {
+                    movieImage
+                        .frame(width: view.size.width*0.2, height: view.size.height * 0.15, alignment: .center)
+                    textMovieDescription()
                 }
+                Spacer()
+                checkmarkCellButtom()
             }
-            Spacer()
-            Image(systemName: "checkmark.circle.fill")
+            .frame(width: view.size.width, height: view.size.height * 0.15, alignment: .center)
+            .foregroundColor(Color("primaryWhite"))
+            .background(Color("primaryBlack"))
         }
     }
 }
+
+var movieImage: some View {
+    Image(systemName: "camera")
+}
+
+struct textMovieDescription: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Edward Scissorhands")
+                    .font(.system(size: 24, weight: .regular, design: .default))
+                Spacer()
+            }
+            HStack {
+                Text("1992")
+                Text("Drama, comedy")
+                Spacer()
+            }
+            .font(.system(size: 16, weight: .regular, design: .default))
+        }
+    }
+}
+
+struct checkmarkCellButtom: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "checkmark.circle.fill")
+            Spacer()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct movieCell_Previews: PreviewProvider {
     static var previews: some View {
